b63ad59b470dabe524d89404fe4749afb87ceca4
Abdul <codehackerr@code.love>
2026-07-07 15:26:46 -0700

    Add multiple error handling scenarios

--- handle-errors/hello/hello.go
+++ handle-errors/hello/hello.go
@@ -8,11 +8,29 @@ import  (
 
 func main() {
 
+    // set prefix
+    log.SetPrefix("Greetings:")
+
+
+    // Decalare and init
     message, err := greetings.Greet("John")
     
-    if(err != nil) {
-        log.Fatal(err)
-    }
-
     fmt.Println(message)
+
+    
+    // Assign
+    message, err = greetings.Greet("")
+
+    if(err != nil) {
+        log.Println("Non fatal: ", err)
+    }
+ 
+    message, err = greetings.Greet("")
+
+    if(err != nil) {
+        log.Fatal("Fatal: ", err)
+    }
+    
+    
+
 }
