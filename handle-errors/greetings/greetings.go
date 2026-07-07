3aa11212ca0c7cd65ee7a64b9a2863addee061e0
Abdul <codehackerr@code.love>
2026-07-07 14:56:11 -0700

    Handle errors: Module returning error

---
+++ handle-errors/greetings/greetings.go
@@ -0,0 +1,16 @@
+package greetings
+
+import ( 
+   "fmt"
+   "errors"
+)
+
+// returns greeting for the named person
+func Greet(name string) (string, error) {
+   
+   if(name == nil) {
+      return "", errors.New("Empty Name")
+   }
+   msg := fmt.Sprintf("Hi %v, great to see you", name)
+   return msg, nil
+}
